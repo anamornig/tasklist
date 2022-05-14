6a637741131b39507e5525f48cc675fb28204f61
anamornig <anamoruja@gmail.com>
2022-05-14 13:20:20 +0200

    mock de tareas

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,31 @@
+import {Task} from "./task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "Terminar primer modulo de Angular",
+        day: 'agosto 5 a las 12',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: "Regar las plantas",
+        day: 'agosto 5 a las 15',
+        reminder: true,        
+
+    },
+    {
+        id: 3,
+        text: "Hacer las compras",
+        day: 'agosto 5 a las 18',
+        reminder: false,        
+
+    },
+    {
+        id: 4,
+        text: "Leer un libro",
+        day: 'agosto 5 a las 20',
+        reminder: false,        
+
+    }
+]
