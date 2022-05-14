eda1e1ea8b1c0e9949f3af23221fe07e6c28177e
anamornig <anamoruja@gmail.com>
2022-05-14 20:03:56 +0200

    componente de lista de items

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+import {Task} from '../../task'
+import {TASKS} from '../../mock-tasks'
+import { faTimes} from "@fortawesome/free-solid-svg-icons";
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+
+  @Input() task: Task = TASKS[0] 
+  faTimes = faTimes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
